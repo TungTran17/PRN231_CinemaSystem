b3baaadcbe92acc2413b5c18083d9487e9451bc9
TungTran17 <tungtvhe163636@fpt.edu.vn>
2024-06-07 23:49:41 +0700

    Model and Layout project

---
+++ CinemaSystemWebClient/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CinemaSystemWebClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
